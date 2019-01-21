09168a279015a4c464f94331b1331f61f1eeabac
Belle0317 <46584373+Belle0317@users.noreply.github.com>
2019-01-21 11:45:55 +0800

    Update main.js
    update

--- main.js
+++ main.js
@@ -1,7 +1,17 @@
 var app = new Vue({
 	el: '#app',
 	data: {
-		role: 'hr',
+		math: 90,
+		engish: 80,
+		physics: 95,
 	},
+	computed: {
+		sum: function() {
+			return parseFloat(this.math) + parseFloat(this.engish) + parseFloat(this.physics);
+		},
+		average: function() {
+			return Math.round(this.sum / 3);
+		}
+	}
 
 });
